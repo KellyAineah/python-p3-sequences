bc3f8d2530d60b09111885efc4b1d427198c639b
KellyAineah <kellyainea99@gmail.com>
2024-05-22 23:20:27 +0300

    added fibonacci series

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+
+    fib_array = [0, 1]
+    for i in range(2, length):
+        
+        next_number = fib_array[-1] + fib_array[-2]
+        
+        fib_array.append(next_number)
+        
+    print(fib_array)
